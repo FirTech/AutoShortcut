ea964bcd895dfbb35c5b67bb9da3f2a80554337d
Fir <834772509@qq.com>
2025-07-31 19:15:45 +0800

    add Debug Type

--- src/console.rs
+++ src/console.rs
@@ -9,6 +9,8 @@ pub enum ConsoleType {
     Warning,
     /// 错误
     Error,
+    /// 调试
+    Debug,
 }
 
 pub fn write_console(console_type: ConsoleType, message: &str) {
@@ -17,6 +19,7 @@ pub fn write_console(console_type: ConsoleType, message: &str) {
         ConsoleType::Success => style("Success").green(),
         ConsoleType::Warning => style("Warning").yellow(),
         ConsoleType::Error => style("Err    ").red(),
+        ConsoleType::Debug => style("Debug  ").magenta()
     };
     println!("  {}      {}", &title, message);
 }
